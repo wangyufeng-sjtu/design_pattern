a95a4a1a25a1b8e063eafb746d0d1e650c41a855
yufeng_wang <wangyufeng@sjtu.edu.cn>
2019-10-24 16:30:58 +0800

    Added Bridge Mode

---
+++ src/Bridge/Client.java
@@ -0,0 +1,12 @@
+package Bridge;
+
+class Client {
+    public static void main(String args[]) {
+        Image image;
+        ImageImp imp;
+        image = (Image)XMLUtil.getBean("image");
+        imp = (ImageImp)XMLUtil.getBean("os");
+        image.setImageImp(imp);
+        image.parseFile("小龙女");
+    }
+}
